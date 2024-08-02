3b7e090e6132a59da2ad6e018560699a0451fdb4
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-08-01 23:06:29 -0300

    refactor: Update background color and add date formatting function

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,3 +1,8 @@
 export function formatCNPJ(cnpj) {
   return cnpj.replace(/\D/g, '');
 }
+
+export function formatDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('pt-BR');
+}
